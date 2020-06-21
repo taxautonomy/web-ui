d7eddd50dcd089f3dc5a876acc7c7f65fb207add
Chamith Siriwardena <chamith@gmail.com>
2020-06-21 09:26:53 +0000

    displaying additional infor in tax calculations

--- src/components/TaxCal/TaxSummaryCard.js
+++ src/components/TaxCal/TaxSummaryCard.js
@@ -9,12 +9,12 @@ import {
   Button,
   CardHeader,
   Avatar,
-  TableCell, 
-  Table, 
-  TableRow, 
-  TableBody
+  TableCell,
+  Table,
+  TableRow,
+  TableBody, useMediaQuery
 } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -38,6 +38,24 @@ const useStyles = makeStyles((theme) => ({
 export default function TaxSummaryCard(props) {
   const { taxSummary } = props;
   const classes = useStyles();
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('xs'));
+
+  const AdditionalInfo = () => {
+    return (
+  <React.Fragment><TableCell>
+  <Typography className={classes.cardTitle} color="textSecondary" gutterBottom>
+    Qualifying Payments (LKR)
+  </Typography>
+  <Typography variant="body2" component="p" style={{ fontWeight: 'bold' }}>{taxSummary ? taxSummary.qp_actual.toFixed(2) : '...'}</Typography>
+</TableCell>
+  <TableCell>
+    <Typography className={classes.cardTitle} color="textSecondary" gutterBottom>
+      Taxable Income (LKR)
+  </Typography>
+    <Typography variant="body2" component="p" style={{ fontWeight: 'bold' }}>{taxSummary ? taxSummary.taxable_income.toFixed(2) : '...'}</Typography>
+  </TableCell></React.Fragment>)
+}
 
   return (
     <Card className={classes.card}>
@@ -55,10 +73,11 @@ export default function TaxSummaryCard(props) {
             <TableRow>
               <TableCell>
                 <Typography className={classes.cardTitle} color="textSecondary" gutterBottom>
-                  Total (LKR)
+                  {taxSummary && taxSummary.tax_total < 0 ? 'Refund to be claimed ' : 'Balance to be paid '}(LKR)
                 </Typography>
-                <Typography variant="body2" component="p" style={{ fontWeight: 'bold' }}>{taxSummary?taxSummary.tax_total.toFixed(2):'...'}</Typography>
+                <Typography variant="body2" component="p" style={{ fontWeight: 'bold' }}>{taxSummary ? taxSummary.tax_total.toFixed(2) : '...'}</Typography>
               </TableCell>
+              {isSmallScreen ? '' : <AdditionalInfo/>}
             </TableRow>
           </TableBody>
         </Table>
